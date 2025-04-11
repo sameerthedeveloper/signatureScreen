930968c98a5c491b660682a26784829d711fe5fc
Mohamed Sameer <md.sameer.dev@gmail.com>
2025-04-11 23:14:12 +0530

    Add input fields for dimensions in inches and update calculation logic

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const panelHeight_ft = document.getElementById("height-ft");
 const panelWidth_ft = document.getElementById("width-ft");
 const panelHeight_mm = document.getElementById("height-mm");
 const panelWidth_mm = document.getElementById("width-mm");
+const panelHeight_in = document.getElementById("height-in");
+const panelWidth_in = document.getElementById("width-in");
 
 function updateFromPanelCount() {
     const hVal = parseInt(hPanels.value);
@@ -22,6 +24,11 @@ function updateFromPanelCount() {
 
     panelHeight_ft.value = (hVal * 337.5).toFixed(2)/304.8 .toFixed(2);
     panelWidth_ft.value = (vVal * 600).toFixed(2)/304.8.toFixed(2);
+
+    panelHeight_in.value = (hVal * 337.5).toFixed(2)/25.4.toFixed(2);
+    panelWidth_in.value = (vVal * 600).toFixed(2)/25.4.toFixed(2);
+
+
 
 
 }
